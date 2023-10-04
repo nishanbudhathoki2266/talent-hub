96a8ebcb3ea4a3f430c6cecc7f90503673ebc018
nishanbudhathoki2266 <nishanbudhathoki2266@gmail.com>
2023-10-04 15:02:22 +0545

    Tested tailwind classes

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const index = () => {
-  return <div>Welcome to Dalay Dai.</div>;
+  return <h1 className="text-3xl font-bold">Welcome to Dalay Dai.</h1>;
 };
 
 export default index;
