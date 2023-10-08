b1cedaa78a9d6a5187f97d97243d170b8690f6f0
nishanbudhathoki2266 <nishanbudhathoki2266@gmail.com>
2023-10-08 12:27:29 +0545

    Created the sign in form

--- client/pages/signin/index.js
+++ client/pages/signin/index.js
@@ -1,7 +1,91 @@
-import React from "react";
+import { useState } from "react";
+import Link from "next/link";
+import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai";
+import OAuth from "@/components/OAuth";
 
 const SignInPage = () => {
-  return <div>SignInPage</div>;
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const { email, password } = formData;
+
+  function onChange(e) {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  }
+
+  return (
+    <section className="bg-gray-3">
+      <h1 className="text-3xl text-center mt-6 font-bold">Sign In</h1>
+
+      <div className="px-6 py-12 max-w-4xl mx-auto">
+        <form>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={onChange}
+            placeholder="Email address"
+            className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"
+          />
+          <div className="relative mb-6">
+            <input
+              type={showPassword ? "text" : "password"}
+              id="password"
+              value={password}
+              onChange={onChange}
+              placeholder="Password"
+              className="w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"
+            />
+            {showPassword ? (
+              <AiFillEyeInvisible
+                className="absolute right-3 top-3 text-xl cursor-pointer"
+                onClick={() => setShowPassword((prevState) => !prevState)}
+              />
+            ) : (
+              <AiFillEye
+                className="absolute right-3 top-3 text-xl cursor-pointer"
+                onClick={() => setShowPassword((prevState) => !prevState)}
+              />
+            )}
+          </div>
+          <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg">
+            <p className="mb-6">
+              Don't have an account?
+              <Link
+                href="/sign-up"
+                className="text-red-600 hover:text-red-700 transition duration-200 ease-in-out ml-1"
+              >
+                Register
+              </Link>
+            </p>
+            <p>
+              <Link
+                href="/forgot-password"
+                className="text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out"
+              >
+                Forgot password?
+              </Link>
+            </p>
+          </div>
+          <button
+            className="w-full bg-blue-600 text-white px-7 py-3 text-sm font-medium uppercase rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800"
+            type="submit"
+          >
+            Sign in
+          </button>
+          <div className="flex items-center  my-4 before:border-t before:flex-1 before:border-gray-300 after:border-t after:flex-1 after:border-gray-300">
+            <p className="text-center font-semibold mx-4">OR</p>
+          </div>
+          <OAuth />
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default SignInPage;
