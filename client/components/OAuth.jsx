23f807ad31dbdb45b7c1d5cd8dee7b00114126da
nishanbudhathoki2266 <nishanbudhathoki2266@gmail.com>
2023-10-14 13:29:39 +0545

    Completed the OAuth functionality

--- client/components/OAuth.jsx
+++ client/components/OAuth.jsx
@@ -1,8 +1,46 @@
 import { FcGoogle } from "react-icons/fc";
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import { toast } from "react-toastify";
+import { doc, getDoc, serverTimestamp, setDoc } from "firebase/firestore";
+import { db } from "@/firebase";
+import { useRouter } from "next/router";
 
 export default function OAuth() {
+  const router = useRouter();
+
+  const handleGoogleAuth = async () => {
+    try {
+      const auth = getAuth();
+
+      // Google Auth Provider
+      const provider = new GoogleAuthProvider();
+
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+
+      // Check if there is a user, if not we add the details and save to the database
+      const docRef = doc(db, "users", user.uid);
+      const docSnap = await getDoc(docRef);
+
+      if (!docSnap.exists()) {
+        await setDoc(docRef, {
+          name: user.displayName,
+          email: user.email,
+          timestamp: serverTimestamp(),
+        });
+      }
+
+      // After the steps, directing user to the home page
+      toast.success("Authentication successfull!");
+      router.push("/");
+    } catch (err) {
+      console.log(err);
+      toast.error("Couldn't authorize with Google!");
+    }
+  };
   return (
     <button
+      onClick={handleGoogleAuth}
       type="button"
       className="flex items-center justify-center w-full bg-red-700 text-white px-7 py-3 uppercase text-sm font-medium hover:bg-red-800 active:bg-red-900 shadow-md hover:shadow-lg active:shadow-lg transition duration-150 ease-in-out rounded"
     >
