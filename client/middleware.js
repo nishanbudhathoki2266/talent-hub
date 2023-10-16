c804e1541e1be814b086bd13129010b0c4a53f8f
nishanbudhathoki2266 <nishanbudhathoki2266@gmail.com>
2023-10-16 13:34:00 +0545

    Added a profile page and rendered name and email

--- client/middleware.js
+++ client/middleware.js
@@ -2,7 +2,8 @@ import { NextResponse } from "next/server";
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request) {
-  const isLoggedIn = Boolean(request.cookies.get("isLoggedIn").value);
+  const isLoggedIn = Boolean(request.cookies?.get("isLoggedIn")?.value);
+
   if (!isLoggedIn) {
     return NextResponse.redirect(new URL("/sign-in", request.url));
   }
