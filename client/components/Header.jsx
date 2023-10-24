6e1c33456c40b99498b4f984c184e85818250585
nishanbudhathoki2266 <nishanbudhathoki2266@gmail.com>
2023-10-24 19:49:38 +0545

    Fixed the active navigation bug

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
@@ -19,33 +18,53 @@ export default function Header() {
         href="/"
         className="text-xl md:text-2xl font-bold tracking-widest cursor-pointer"
       >
-        <h2 className="uppercase text-black tracking-tighter">
+        <h2 className="uppercase text-black border-b-2 border-spacing-2 tracking-tighter">
           Talent <span className="text-secondary">Hub</span>
         </h2>
       </Link>
       <div>
         <ul className="flex space-x-10">
           <li
-            className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
-              matchPath("/") ? "text-black border-b-red-500" : ""
-            }`}
+            className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent `}
           >
-            <Link href="/">Home</Link>
+            <Link
+              href="/"
+              className={`${
+                matchPath("/")
+                  ? "text-black border-b-2 border-spacing-2 border-b-red-500"
+                  : ""
+              }`}
+            >
+              Home
+            </Link>
           </li>
           <li
-            className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
-              matchPath("/sign-in") ? "text-black border-b-red-500" : ""
-            }"
-            }`}
+            className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent`}
           >
-            <Link href="/sign-in">Sign In</Link>
+            <Link
+              href="/sign-in"
+              className={`${
+                matchPath("/sign-in")
+                  ? "text-black border-b-2 border-spacing-2 border-b-red-500"
+                  : ""
+              }`}
+            >
+              Sign In
+            </Link>
           </li>
           <li
-            className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
-              matchPath("/profile") ? "text-black border-b-red-500" : ""
-            }`}
+            className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent `}
           >
-            <Link href="/profile">Profile</Link>
+            <Link
+              href="/profile"
+              className={`${
+                matchPath("/profile")
+                  ? "text-black border-b-2 border-spacing-2 border-b-red-500"
+                  : ""
+              }`}
+            >
+              Profile
+            </Link>
           </li>
         </ul>
       </div>
