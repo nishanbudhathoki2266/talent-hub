df448f208f0ef09824ee82212379182b9f7d4205
nishanbudhathoki2266 <nishanbudhathoki2266@gmail.com>
2023-10-27 10:39:52 +0545

    Extracted the card component to another file

---
+++ client/components/TalentCard.jsx
@@ -0,0 +1,57 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const TalentCard = ({ talent }) => {
+  return (
+    <article className="py-6 px-4 bg-gray-200 hover:bg-gray-300 hover:-translate-y-1 transition-all ease-out duration-300 rounded flex flex-wrap items-center justify-between md:flex-nowrap gap-4">
+      <span className="md:w-48 aspect-square md:mb-0 mb-6 flex-shrink-0 relative rounded">
+        <Image
+          src={talent.profileImageUrl || "/assets/default-user.png"}
+          alt={`${talent.fullName}'s picture`}
+          layout="fill"
+          objectFit="cover"
+        />
+      </span>
+      <p className="md:flex-grow">
+        <h2 className="text-2xl font-semibold text-gray-900 mb-2 tracking-wide">
+          {talent.fullName}
+        </h2>
+        <p className="leading-relaxed font-medium">
+          Glossier echo park pug, church-key sartorial biodiesel vexillologist
+          pop-up snackwave ramps cornhole. Marfa 3 wolf moon party messenger bag
+          selfies, poke vaporware kombucha lumbersexual pork belly polaroid
+          hoodie portland craft beer.
+        </p>
+        <span className="text-gray-500 block font-extralight italic text-sm mt-2">
+          Joined on{" "}
+          {new Date(talent.timestamp.seconds * 1000).toLocaleString("en-US", {
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+          })}
+        </span>
+        <Link
+          className="text-indigo-500 inline-flex items-center mt-2"
+          href="#"
+        >
+          Learn More
+          <svg
+            className="w-4 h-4 ml-2"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            stroke-width="2"
+            fill="none"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <path d="M5 12h14"></path>
+            <path d="M12 5l7 7-7 7"></path>
+          </svg>
+        </Link>
+      </p>
+    </article>
+  );
+};
+
+export default TalentCard;
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "@/firebase";
-import Image from "next/image";
+import TalentCard from "@/components/TalentCard";
 
 const Index = () => {
   const [talents, setTalents] = useState([]);
@@ -19,67 +19,16 @@ const Index = () => {
         }
       });
 
-      console.log("HELLO FROM USEEFFECT");
-
       setTalents(talentsData);
     };
 
     fetchUsers();
   }, []);
 
-  console.log(talents);
-
   return (
     <section className="text-gray-600 divide-y-8 divide-white body-font overflow-hidden container py-4 mx-auto">
       {talents?.map((talent) => (
-        <div className="py-6 px-4 bg-gray-200 hover:bg-gray-300 hover:-translate-y-1 cursor-pointer transition-all ease-out duration-300 rounded flex flex-wrap items-center justify-between md:flex-nowrap gap-4">
-          <div className="md:w-48 aspect-square md:mb-0 mb-6 flex-shrink-0 relative rounded">
-            <Image
-              src={talent.profileImageUrl || "/assets/default-user.png"}
-              alt={`${talent.fullName}'s picture`}
-              layout="fill"
-              objectFit="cover"
-            />
-          </div>
-          <div className="md:flex-grow">
-            <h2 className="text-2xl font-semibold text-gray-900 mb-2 tracking-wide">
-              {talent.fullName}
-            </h2>
-            <p className="leading-relaxed font-medium">
-              Glossier echo park pug, church-key sartorial biodiesel
-              vexillologist pop-up snackwave ramps cornhole. Marfa 3 wolf moon
-              party messenger bag selfies, poke vaporware kombucha lumbersexual
-              pork belly polaroid hoodie portland craft beer.
-            </p>
-            {console.log(talent.timestamp)}
-            <span className="text-gray-500 block font-extralight italic text-sm mt-2">
-              Joined on{" "}
-              {new Date(talent.timestamp.seconds * 1000).toLocaleString(
-                "en-US",
-                {
-                  year: "numeric",
-                  month: "long",
-                  day: "numeric",
-                }
-              )}
-            </span>
-            <a className="text-indigo-500 inline-flex items-center mt-2">
-              Learn More
-              <svg
-                className="w-4 h-4 ml-2"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                stroke-width="2"
-                fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              >
-                <path d="M5 12h14"></path>
-                <path d="M12 5l7 7-7 7"></path>
-              </svg>
-            </a>
-          </div>
-        </div>
+        <TalentCard key={talent.uid} talent={talent} />
       ))}
     </section>
   );
