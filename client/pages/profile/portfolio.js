124c0a4196d7a91121e70fc4d8aa0d4e9bd663cc
nishanbudhathoki2266 <nishanbudhathoki2266@gmail.com>
2023-10-29 20:18:08 +0545

    Completed creating form for adding experience

--- client/pages/profile/portfolio.js
+++ client/pages/profile/portfolio.js
@@ -6,20 +6,29 @@ const AddPortfolioPage = () => {
   const {
     register,
     handleSubmit,
+    setValue,
+    getValues,
     formState: { errors },
   } = useForm();
 
   const [experiences, setExperiences] = useState([]);
+
+  const [showExperienceForm, setShowExperienceForm] = useState(true);
+
+  const toggleExperienceForm = () => {
+    setShowExperienceForm((currState) => !currState);
+  };
+
   const [skills, setSkills] = useState([]);
 
-  const addExperience = () => {
-    setExperiences([
-      ...experiences,
-      { duration: "", position: "", description: "" },
-    ]);
+  console.log("STATE EXP: ", experiences);
+
+  const addExperience = ({ duration, position, description }) => {
+    setExperiences([...experiences, { duration, position, description }]);
   };
 
   const removeExperience = (index) => {
+    console.log("You clicked index: ", index);
     setExperiences(experiences.filter((_, i) => i !== index));
   };
 
@@ -28,19 +37,17 @@ const AddPortfolioPage = () => {
   };
 
   const removeSkill = (index) => {
-    setSkills(skills.filter((_, i) => i !== index));
+    setSkills(skills.filter((_, i) => i !== index + 1));
   };
 
   const onSubmit = (data) => {
     // Submit the data to your backend
-    console.log(data);
-    const formattedData = {
-      experiences: experiences.map((experience) => ({
-        year: experience.year,
-        work: experience.work,
-      })),
-      skills: skills,
-    };
+    // console.log(data);
+
+    console.log({
+      ...data,
+      experiences: experiences.filter((exp) => exp.hasOwnProperty("duration")),
+    });
 
     // TODO: Submit formattedData to your backend
   };
@@ -78,42 +85,82 @@ const AddPortfolioPage = () => {
           />
           {errors.bio && <p>{errors.bio.message}</p>}
         </div>
-        <div>
-          <label>Experiences</label>
-          <ul>
-            {experiences.map((_, index) => (
-              <li key={index}>
-                <input
-                  type="number"
-                  placeholder="Work Duration (in years)"
-                  {...register(`experiences.${index}.duration`, {
-                    required: "Work duration is required",
-                  })}
-                />
-                <input
-                  type="text"
-                  placeholder="Your Position"
-                  {...register(`experiences.${index}.position`, {
-                    required: "Position is required",
-                  })}
-                />
-                <textarea
-                  type="text"
-                  placeholder="Work Description"
-                  {...register(`experiences.${index}.description`, {
-                    required: "Description is required",
-                  })}
-                />
-                <button type="button" onClick={() => removeExperience(index)}>
-                  Remove
-                </button>
-              </li>
-            ))}
-          </ul>
-          <button type="button" onClick={addExperience}>
-            Add Experience
-          </button>
-        </div>
+        {showExperienceForm && (
+          <div className="flex flex-col">
+            <label className="text-md font-medium tracking-wide text-gray-500">
+              Experiences (Add as many you want - One at a time)
+            </label>
+
+            <div className="w-full flex flex-wrap justify-between items-center">
+              <input
+                type="number"
+                placeholder="Work Duration (in years)"
+                {...register(`duration`, {
+                  required: "Work duration is required",
+                })}
+                className="mb-6 px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300"
+              />
+              <input
+                type="text"
+                placeholder="Your Position"
+                {...register(`position`, {
+                  required: "Position is required",
+                })}
+                className="mb-6 px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300"
+              />
+            </div>
+            <textarea
+              type="text"
+              placeholder="Work Description"
+              {...register(`description`, {
+                required: "Description is required",
+              })}
+              className="mb-6 w-full min-h-[12vh] px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300"
+            />
+
+            <button
+              type="button"
+              onClick={() => {
+                const duration = getValues("duration");
+                const position = getValues("position");
+                const description = getValues("description");
+                if (!duration || !position || !description) return;
+                addExperience({
+                  duration,
+                  position,
+                  description,
+                });
+                setValue("duration", "");
+                setValue("position", "");
+                setValue("description", "");
+                setShowExperienceForm(false);
+              }}
+              className="bg-blue-600 text-white uppercase px-7 py-3 text-sm font-medium rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800 mb-2"
+            >
+              Add
+            </button>
+          </div>
+        )}
+
+        <button
+          type="button"
+          className="w-full bg-blue-600 text-white uppercase px-7 py-3 text-sm font-medium rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800 mb-2"
+          onClick={() => toggleExperienceForm()}
+        >
+          {showExperienceForm
+            ? "Don't wanna Add?"
+            : experiences.length > 0
+            ? "Add More"
+            : "Add Experience"}
+        </button>
+
+        <h2 className="text-2xl font-bold">Your Experiences</h2>
+        {experiences
+          .filter((exp) => exp.hasOwnProperty("duration"))
+          .map((filteredExp) => (
+            <p>{filteredExp.position}</p>
+          ))}
+
         <div>
           <label>Skills</label>
           <ul>
